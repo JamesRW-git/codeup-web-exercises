827ca24d6693225623de954fa2aa5cefba51f426
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-03 18:42:43 -0600

    add JS exercises

---
+++ js/external.js
@@ -0,0 +1,79 @@
+"use strict"
+
+console.log("Hello from external Javascript!");
+
+//First Exercise
+/*
+alert("Welcome to my website!");
+
+var userFavColor = prompt("What's your favorite color?");
+
+alert("Really? " + userFavColor + " is my favorite color, too!");*/
+
+//Second Exercise
+
+/*let dailyPrice = 3.00;
+let lilMermaid = Number(prompt("How many days do you want to keep The Little Mermaid?")) * dailyPrice;
+let broBear = Number(prompt("How many days do you want to keep Brother Bear?")) * dailyPrice;
+let hercules = Number(prompt("How many days do you want to keep Hercules?")) * dailyPrice;
+
+let totalCost = lilMermaid + broBear + hercules;
+
+alert("Thank you! Your total will be $" + totalCost);*/
+
+//Third Exercise
+
+/*let googleTime = Number(prompt("How many hours will you work for Google this week?")) * 400;
+let amazonTime = Number(prompt("How many hours will you work for Amazon this week?")) * 380;
+let metaTime = Number(prompt("How many hours are you going to worship The Zuc this week?")) * 350;
+let totalEarn = googleTime + amazonTime + metaTime;
+alert("You will earn $" + totalEarn + " this week.");*/
+
+//Fourth Exercise
+
+/*let maxClassSize = Number(prompt("What's the max class size?"));
+console.log(maxClassSize);
+let currentClassSize = Number(prompt("How many people are in the class currently?")) + 1;
+console.log(currentClassSize);
+let conflictOrNo = confirm("Click OK if this class does not conflict with any other scheduled class.");
+console.log(conflictOrNo);
+
+if (conflictOrNo && (currentClassSize <= maxClassSize)) {
+    alert("Congratulations, we'll see you in class!");
+    console.log("Success");
+} else {
+    alert("Sorry, this class will not work for you.");
+    console.log("Fail");
+}*/
+
+//Fifth Exercise
+
+let productMin = 2;
+let productsInCart = Number(prompt("How many items are you getting?"));
+let expirationDate = prompt("What is the expiration date of the offer you'd like to use? (MM/DD/YYYY)");
+let inputDate = new Date(expirationDate);
+let todaysDate = new Date();
+
+//Check Date
+if(inputDate.setHours(0,0,0,0) <= todaysDate.setHours(0,0,0,0)) {
+    var expired = true;
+} else {
+    expired = false;
+}
+
+//Check Membership
+let premiumMember = confirm("Click OK if you are a Premium Member");
+
+//Final Check and User Alert
+if (premiumMember || !expired && productMin < productsInCart) {
+    console.log("Accepted");
+    alert("Everything looks good! Here's some free money.");
+} else {
+    console.log("Invalid");
+    alert("Something is wrong, no discount for you.");
+}
+
+
+
+
+
---
+++ js/my-first-js-file.js
@@ -0,0 +1,30 @@
+"use strict"
+
+// console.log("This is fine.");
+//
+// let myName = "James";
+//
+// console.log(myName);
+//
+// myName += " Woodward";
+//
+// console.log(myName);
+
+// Allows the user's name to be logged as myName
+// let myName = prompt("Hi, what's your name?");
+
+// console.log(myName);
+
+// myName += " " + prompt("What is your last name?");
+
+// console.log(myName.toUpperCase());
+
+let isAwesome = confirm("Are you awesome today?");
+
+while(!isAwesome) {
+    isAwesome = confirm("ARE YOU AWESOME TODAY?");
+}
+
+if (isAwesome) {
+    console.log("Always good to hear! Stay awesome or else");
+}
