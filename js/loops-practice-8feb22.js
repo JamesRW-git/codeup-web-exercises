4c9f0700799fb4c00d45947c5e82f0bc2ea5f891
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-09 15:27:05 -0600

    added loops-practice-8feb22.js and do while loops classroom example

--- js/loops-practice-8feb22.js
+++ js/loops-practice-8feb22.js
@@ -1,7 +1,7 @@
 "use strict";
 //follow along during Loop lecture. Thanks Casey!
 
-let isAwesome = true;
+//let isAwesome = true;
 //if(isAwesome) {
 //    console.log('I am awesome');
 //}
@@ -13,8 +13,38 @@ let isAwesome = true;
 //}
 //console.log('The loop has ended');
 
-//To do: use a while loop to output the numbers from 8-50
-let incrementor = 8;
-while (incrementor <= 50) {
-    console.log(incrementor++)
-}
+//To do: use a while loop to output the numbers from 8-50 COMPLETE
+//To do: using the same example, output the odd numbers from 8-50
+//let incrementor = 8;
+//while (incrementor < 50) {
+//    incrementor++;
+//    if(incrementor % 2 === 0){
+//        console.log(incrementor);
+//    } else {
+//        continue;
+//    }
+//}
+
+//full name example
+//let fullNameEntered = false;
+//let names = "";
+////We don't know how many names they have
+//do {
+//    names += prompt('Please enter your name' + " ");
+//    fullNameEntered = confirm('Is this your full name?');
+//    if (!fullNameEntered){
+//        names += " ";
+//    }
+//} while (!fullNameEntered)
+//
+//alert('This is your name: ' + names);
+
+let numberOfNames = prompt("How many names do you use?");
+let names = "";
+do {
+    names += prompt('Please enter name ' + ' ');
+    names += ' ';
+    numberOfNames--;
+} while (numberOfNames > 0);
+
+alert('This is your name: ' + names);
