c93c55a15e790eafcf42ddd4c708e14a11914c8d
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-09 14:11:55 -0600

    add and complete exercise for showMultiplicationTable(num)

---
+++ js/loops.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//Exercise For Loops 2
+function showMultiplicationTable(num){
+    for (var i = 1; i <= 10; i++) {
+        console.log(num + ' multiplied by ' + i + ' is ' + (num * i));
+    }
+}
+
+showMultiplicationTable(7);
+showMultiplicationTable(1);
+showMultiplicationTable(79);
