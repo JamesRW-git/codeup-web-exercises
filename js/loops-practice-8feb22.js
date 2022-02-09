de57a642eabfe0a7d4c0d53fed6450f70e767f35
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-09 14:33:05 -0600

    add loops-practice-8feb22.js and follow along exercises

---
+++ js/loops-practice-8feb22.js
@@ -0,0 +1,20 @@
+"use strict";
+//follow along during Loop lecture. Thanks Casey!
+
+let isAwesome = true;
+//if(isAwesome) {
+//    console.log('I am awesome');
+//}
+
+//this loop never runs
+//let incrementor = 0;
+//while (incrementor > 0) {
+//    console.log(incrementor--);
+//}
+//console.log('The loop has ended');
+
+//To do: use a while loop to output the numbers from 8-50
+let incrementor = 8;
+while (incrementor <= 50) {
+    console.log(incrementor++)
+}
