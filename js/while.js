6ba9e94112e701b6d96929ce56d3aea7863eced5
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-10 10:05:36 -0600

    fixed code for do while loop exercise to not return a negative number of remaining inventory and log iterations all the way down to 0 remaining

--- js/while.js
+++ js/while.js
@@ -15,12 +15,14 @@ while (numInput <= 65536) {
 //use inside code: Math.floor(Math.random() * 5) + 1
 var allCones = Math.floor(Math.random() * 50) + 50;
 
-
 do {
         var randomNumber = Math.floor(Math.random() * 5) + 1;
-        console.log(randomNumber + ' cones sold...I have ' + allCones + ' left.');
-        allCones -= randomNumber;
-} while (allCones - randomNumber >= 0);
+        if(randomNumber > allCones) {
+                console.log('I cannot sell you ' + randomNumber + ' cones. I only have ' + allCones + ' left.');
+        } else {
+                allCones -= randomNumber;
+                console.log(randomNumber + ' cones sold...I have ' + allCones + ' left.');
+        }
+} while (allCones >= randomNumber);
 
-console.log('I cannot sell you ' + randomNumber + ' cones. I only have ' + allCones + ' left.');
 console.log('Yay! I sold them ALL!');
