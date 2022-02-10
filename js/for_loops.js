3fbe07d46ef3d80f777bd54cafcbf7b68a5a1631
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-09 20:46:05 -0600

    refactored getRandomNumber function to include minRandom parameter instead of hardcoded input

--- js/for_loops.js
+++ js/for_loops.js
@@ -10,12 +10,12 @@ function showMultiplicationTable(num){
 showMultiplicationTable(79);
 
 //Exercise For Loops 3
-function getRandomNumber(maxRandom) {
-    return Math.floor(Math.random() * (maxRandom - 20) + 20);
+function getRandomNumber(maxRandom , minRandom) {
+    return Math.floor(Math.random() * (maxRandom - minRandom) + minRandom);
 }
 
 for(var r = 1; r <= 10; r++) {
-    let rando = getRandomNumber(200);
+    let rando = getRandomNumber(200, 20);
     if(rando % 2 === 0) {
         console.log(rando + ' is even');
 } else {
@@ -26,7 +26,6 @@ for(var r = 1; r <= 10; r++) {
 //Exercise For Loops 4
 //for(var i = 1; i <= 9; i++) {
 //    let numChars = i;
-//    if()
 //}
 
 
