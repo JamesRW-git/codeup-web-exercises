f644ba6c7370e60e4b0a73d62454d89fae96f601
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-10 16:55:04 -0600

    add arrays-practice-10feb22.js for lecture follow along

---
+++ js/arrays-practice-10feb22.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let myNiceArr = ['James', 'Haley', 'Tyler', 'Martin'];
+
+//Notice getting the length for the condition in the for loop
+for (let i = 0; i < myNiceArr.length; i++){
+    console.log('Hello, ' + myNiceArr[i] + ', you\'re the best!');
+}
