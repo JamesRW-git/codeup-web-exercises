5014a5254b833d1e750ac2d252f2cf1cc78d8109
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-09 21:56:24 -0600

    added code for numberTree but it does not work right

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,38 +1,79 @@
 "use strict";
 //Exercise 1 was making for_loops.js and linking it to loops.html
 //Exercise For Loops 2
-function showMultiplicationTable(num){
-    for (var i = 1; i <= 10; i++) {
-        console.log(num + ' multiplied by ' + i + ' is ' + (num * i));
-    }
-}
-
-showMultiplicationTable(79);
-
-//Exercise For Loops 3
-function getRandomNumber(maxRandom , minRandom) {
-    return Math.floor(Math.random() * (maxRandom - minRandom) + minRandom);
-}
-
-for(var r = 1; r <= 10; r++) {
-    let rando = getRandomNumber(200, 20);
-    if(rando % 2 === 0) {
-        console.log(rando + ' is even');
-} else {
-        console.log(rando + ' is odd');
-    }
-}
-
-//Exercise For Loops 4
-//for(var i = 1; i <= 9; i++) {
-//    let numChars = i;
-//}
-
-
-
-
-
-//    for (var i = 20; i < 200; i++) {
-//
+//function showMultiplicationTable(num){
+//    for (var i = 1; i <= 10; i++) {
+//        console.log(num + ' multiplied by ' + i + ' is ' + (num * i));
 //    }
 //}
+//
+//showMultiplicationTable(79);
+//
+////Exercise For Loops 3
+//function getRandomNumber(maxRandom , minRandom) {
+//    return Math.floor(Math.random() * (maxRandom - minRandom) + minRandom);
+//}
+
+//for(var r = 1; r <= 10; r++) {
+//    let rando = getRandomNumber(200, 20);
+//    if(rando % 2 === 0) {
+//        console.log(rando + ' is even');
+//} else {
+//        console.log(rando + ' is odd');
+//    }
+//}
+
+//Exercise For Loops 4
+function numberTree(height) {
+    for (let i = 1; i <= height ; i++) {
+        let tree = i;
+        let space ='' + i;
+
+        for (var j = 1; j <= i; j++) {
+            tree = i.toString();
+        }
+
+        let gap = space.repeat(height + 1 - i);
+            tree = tree + gap;
+        console.log(tree);
+    }
+}
+
+numberTree(9);
+
+
+
+
+//    function drawTree(height) {
+//    for ( let i = 0; i < height ; i++ ) {
+//        let star = '*';
+//        let space = '\n';
+//
+//        for ( let j = 1; j <= i; j++ ) {
+//            star = star + '**';
+//        }
+//
+//        let gap = space.repeat(height-i+1);
+//        star = gap + star;
+//        console.log(star);
+//    }
+//}
+//
+//drawTree(10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
