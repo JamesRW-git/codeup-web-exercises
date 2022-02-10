565b2341e10be08c71982410f92fb92600394cdc
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-10 09:27:47 -0600

    fixed do while loop exercise to reflect random number inside loop

--- js/while.js
+++ js/while.js
@@ -2,21 +2,22 @@
 
 //While Loop Exercise
 
-//let numInput = 2;
-//while (numInput <= 65536) {
-//    if(numInput % 2 === 0) {
-//        console.log(numInput);
-//        numInput *= 2;
-//    }
-//}
+let numInput = 2;
+while (numInput <= 65536) {
+    if(numInput % 2 === 0) {
+        console.log(numInput);
+        numInput *= 2;
+    }
+}
 
 //Do While Loop Exercise
 
 //use inside code: Math.floor(Math.random() * 5) + 1
 var allCones = Math.floor(Math.random() * 50) + 50;
-var randomNumber = Math.floor(Math.random() * 5) + 1;
+
 
 do {
+        var randomNumber = Math.floor(Math.random() * 5) + 1;
         console.log(randomNumber + ' cones sold...I have ' + allCones + ' left.');
         allCones -= randomNumber;
 } while (allCones - randomNumber >= 0);
