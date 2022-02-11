c52d211bdeeb9225f875c3a232728cea2bf2de3c
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-11 16:40:11 -0600

    add code to complete exercise

--- js/planets-array.js
+++ js/planets-array.js
@@ -21,22 +21,31 @@
      */
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
+    planets.unshift('The Sun');
     console.log(planets);
 
     console.log('Adding "Pluto" to the end of the planets array.');
+    planets.push('Pluto');
     console.log(planets);
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
+    planets.shift(planets[1]);
     console.log(planets);
 
     console.log('Removing "Pluto" from the end of the planets array.');
+    planets.pop();
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    console.log('The index of Earth is: ' + planets.indexOf('Earth'));
 
     console.log("Reversing the order of the planets array.");
+    planets.reverse();
     console.log(planets);
+    planets.reverse();
+
 
     console.log("Sorting the planets array.");
+    planets.sort();
     console.log(planets);
 })();
