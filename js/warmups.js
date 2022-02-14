23b8e5e16cf82bc80aa6ff71b6d0b9adb14ce9bf
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-14 09:17:23 -0600

    create warmups.js and code for morning warmup

---
+++ js/warmups.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+//Create a function called findAverage that finds the average of integers
+//across an array
+
+function findAverage(array){
+    var total = 0;
+
+    array.forEach(function(item,index) {
+        total += item;
+    });
+    return total / (array.length);
+}
+
+console.log(findAverage([50, 75, 89, 109, 68]));
