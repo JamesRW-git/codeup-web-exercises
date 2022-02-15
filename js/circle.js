a010295eade39bafb3e827e4f4379b43ca42ae0e
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-15 16:10:46 -0600

    add code to complete exercise

--- js/circle.js
+++ js/circle.js
@@ -5,32 +5,37 @@
     var circle = {
         radius: 3,
 
+
         getArea: function () {
             // TODO: complete this method
+            return Math.PI * Math.pow(circle.radius, 2);
             // hint: area = pi * radius^2
 
-            return; // TODO: return the proper value
+            // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
+            if (doRounding === true) {
+                console.log(Math.round(circle.getArea()));;
+            }
             // TODO: complete this method.
 
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
         }
     };
 
     // log info about the circle
-    console.log("Raw circle information");
+    console.log("Radius: " + circle.radius + ' Area: ' + circle.getArea());
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
     circle.logInfo(true);
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
