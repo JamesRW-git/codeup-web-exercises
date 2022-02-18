aaee8f1d405aabe4ee3623d5531c7ce8303b7ec3
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-18 11:37:04 -0600

    add code for morning warmup

--- js/warmups.js
+++ js/warmups.js
@@ -18,54 +18,78 @@
 
 //Warmup 16 February 2022
 
-//Defines a contact info object
-function ContactInfo(phoneNumber, streetAddress, city, state, postalCode, emailAddress) {
-    this.phoneNumber = phoneNumber;
-    this.streetAddress = streetAddress;
-    this.city = city;
-    this.state = state;
-    this.postalCode = postalCode;
-    this.emailAddress = emailAddress;
-}
+// //Defines a contact info object
+// function ContactInfo(phoneNumber, streetAddress, city, state, postalCode, emailAddress) {
+//     this.phoneNumber = phoneNumber;
+//     this.streetAddress = streetAddress;
+//     this.city = city;
+//     this.state = state;
+//     this.postalCode = postalCode;
+//     this.emailAddress = emailAddress;
+// }
+//
+// //Defines a person object
+// function Person(occupation, dob, contactInfo) {
+//     //assign a property the value of a function
+//     //this helps get the names of a person, but is not executed until you invoke getNames
+//     this.getNames = getNames;
+//     //Then we actually INVOKE this.getNames() so the names are given when the person object is created
+//     this.names = this.getNames();
+//     this.occupation = occupation;
+//     this.dob = dob;
+//     this.contactInfo = contactInfo;
+// }
+//
+// //Define HOW a user gives their name(s) and returns those names as a string array
+// function getNames() {
+//     let namesArr = [];
+//     let hasMoreNames = true;
+//     while (hasMoreNames) {
+//         //Get the names
+//         namesArr.push(prompt('Please enter your name'));
+//         //Then, if they want more names keep looping, if not break loop
+//         hasMoreNames = confirm('Would you like to enter another name?');
+//     }
+//     return namesArr;
+// }
+//
+// //Make new person object using the object functions we defined above
+// let person2 = new Person(
+//     'Fishmonger',
+//     '04.25.1986',
+//     new ContactInfo(
+//         '123.456.7890',
+//         '9999 Last St',
+//         'LA',
+//         '70119',
+//         'brokenemail@reliablemail.com')
+//     );
+//
+// //Log the object so we can see the values in the console
+// console.log(person2);
 
-//Defines a person object
-function Person(occupation, dob, contactInfo) {
-    //assign a property the value of a function
-    //this helps get the names of a person, but is not executed until you invoke getNames
-    this.getNames = getNames;
-    //Then we actually INVOKE this.getNames() so the names are given when the person object is created
-    this.names = this.getNames();
-    this.occupation = occupation;
-    this.dob = dob;
-    this.contactInfo = contactInfo;
-}
+//Warmup 18 February 2022
+//Create a function which returns true if the given argument is a number, false if it is not a number
 
-//Define HOW a user gives their name(s) and returns those names as a string array
-function getNames() {
-    let namesArr = [];
-    let hasMoreNames = true;
-    while (hasMoreNames) {
-        //Get the names
-        namesArr.push(prompt('Please enter your name'));
-        //Then, if they want more names keep looping, if not break loop
-        hasMoreNames = confirm('Would you like to enter another name?');
+function isANum (input) {
+    if (isNaN(parseFloat(input))) {
+    return false;
+    } else {
+        return true;
     }
-    return namesArr;
 }
 
-//Make new person object using the object functions we defined above
-let person2 = new Person(
-    'Fishmonger',
-    '04.25.1986',
-    new ContactInfo(
-        '123.456.7890',
-        '9999 Last St',
-        'LA',
-        '70119',
-        'brokenemail@reliablemail.com')
-    );
+let o = {}
 
-//Log the object so we can see the values in the console
-console.log(person2);
+console.log(isANum('Here\'s a string to test'));
+console.log(isANum('5'));
+console.log(isANum(5));
+console.log(isANum(-78));
+console.log(isANum(true));
+console.log(isANum(''));
+console.log(isANum(null));
+console.log(isANum(['array', 'test']));
+console.log(isANum(o));
+
 
 
