2adb4db66bc821ee15dda1621bfcd09f075f45f9
James Woodward <jamesrobertwoodward@gmail.com>
2022-03-11 11:19:23 -0600

    add code to finish exercises up through jQuery selectors

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,5 +1,15 @@
 "use strict";
 
-$(document).ready(function () {
+$('li').css('font-size', '20px');
 
+$('li, p, h1').css('color', '#870481');
+
+let headerAlertVar = $('h1').html();
+
+$('h1').click(function(){
+    alert(headerAlertVar);
 })
+
+$('#theList').css('border', 'blue dashed .25rem');
+
+$('label').css('color', '#289448');
