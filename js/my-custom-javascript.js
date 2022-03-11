ca015e6bfe5a1176912c969cbfd6b4522fea2b37
James Woodward <jamesrobertwoodward@gmail.com>
2022-03-11 15:47:07 -0600

    complete code for mouse events exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,15 +1,38 @@
 "use strict";
+$(document).ready(function () {
+    // $('li').css('font-size', '20px');
+    //
+    // $('li, p, h1').css('background-color', '#fcf98b');
+    //
+    // let headerAlertVar = $('h1').html();
+    //
+    // $('h1').click(function () {
+    //     alert(headerAlertVar);
+    // })
+    //
+    // $('#theList').css('border', 'blue dashed .25rem');
+    //
+    // $('label').css('background-color', '#289448');
+    $('h1').click(function () {
+        if ($(this).css('background-color') === 'rgba(0, 0, 0, 0)') {
+            $(this).css('background-color', 'rgba(252, 249, 139)');
+        } else {
+            $(this).css('background-color', 'rgba(0, 0, 0, 0)');
+        }
+    });
 
-$('li').css('font-size', '20px');
+    $('p').dblclick(function () {
+        if ($(this).css('font-size') === '16px') {
+            $(this).css('font-size', '12px');
+        } else {
+            $(this).css('font-size', '16px');
+        }
+    });
 
-$('li, p, h1').css('background-color', '#fcf98b');
-
-let headerAlertVar = $('h1').html();
-
-$('h1').click(function(){
-    alert(headerAlertVar);
+    $('li').hover(function(){
+        $(this).css('color', 'rgba(166, 2, 2)');
+    },
+        function(){
+        $(this).css('color', 'rgba(0, 0, 0)');
+        });
 })
-
-$('#theList').css('border', 'blue dashed .25rem');
-
-$('label').css('backgcolor', '#289448');
