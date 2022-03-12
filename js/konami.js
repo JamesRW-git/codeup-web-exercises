10d08b3d6d3563d21991f2bf7c900484a13083a7
James Woodward <jamesrobertwoodward@gmail.com>
2022-03-11 19:34:12 -0600

    add code for konami exercise

--- js/konami.js
+++ js/konami.js
@@ -1,6 +1,8 @@
 "use strict";
 
 $(document).ready(function(){
+    // $('*').css('border', 'red solid 1px')
+
     $('#enter').css('background-color', 'black').css('color', 'white');
 
     let konamillionaire = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
@@ -12,7 +14,11 @@ $(document).ready(function(){
         if(expectedKey === keyPressed) {
             konamiPosition++;
             if (konamiPosition === konamillionaire.length) {
-                alert('30 LIVES MANE');
+                // alert('+30 LIVES, MANE. PLAY HARD')
+                $('#konamiCode').fadeOut(10000, function(){
+                    $('#konamiCode').addClass('d-none');
+                    $('#hiddenSnoop').removeClass('d-none');
+                });
                 konamiPosition = 0;
             }
         } else {
